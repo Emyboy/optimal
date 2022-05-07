3a5013321bf78bf5750eaafb4b60125344f5129f
Chukwuemeka Ifeora <chukwuemekaifeora@gmail.com>
2022-05-07 01:35:58 +0100

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 
-import { Header, Main, Footer, Cards } from "@components";
 
 const Home: React.FC = () => {
     return (
-        <div className="d-flex flex-column min-vh-100">
-            <Header />
-            <Main />
-            <Cards />
-            <Footer />
+        <div>
+            <h1>Home Page</h1>
         </div>
     );
 };
